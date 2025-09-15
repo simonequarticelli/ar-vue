e95719d6cfa496650bea5f1b01234f9d719ee18c
Simone <simone@quattrolinee.it>
2025-09-15 17:17:54 +0200

    Update AR entity scale and rotation for improved marker alignment.

--- src/components/SceneAR.vue
+++ src/components/SceneAR.vue
@@ -7,9 +7,9 @@
       <a-marker type="pattern" url="/pattern-marker.patt">
         <a-entity
             gltf-model="/coin.glb"
-            scale="0.2 0.2 0.2"
+            scale="0.3 0.3 0.3"
             position="0 0 0"
-            rotation="0 0 90"
+            rotation="0 0 180"
             animation-mixer>
         </a-entity>
       </a-marker>
