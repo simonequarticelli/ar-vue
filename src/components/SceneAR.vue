0df66fcd9e6cd263460e05aa42f1764377f63d5d
Simone <simone@quattrolinee.it>
2025-09-15 17:47:34 +0200

    Adjust AR entity scale, position, and rotation for improved marker alignment.

--- src/components/SceneAR.vue
+++ src/components/SceneAR.vue
@@ -7,8 +7,9 @@
       <a-marker type="pattern" url="/pattern-marker.patt">
         <a-entity
             gltf-model="/eva.glb"
-            scale="0.3 0.3 0.3"
-            position="0 0 180"
+            scale="0.2 0.2 0.2"
+            position="0 0 0"
+            rotation="0 180 90"
             animation-mixer>
         </a-entity>
       </a-marker>
