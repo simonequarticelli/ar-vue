4db324454f568b56caaba672cfbf55bfc80ded8e
Simone <simone@quattrolinee.it>
2025-09-15 17:58:43 +0200

    Update AR entity rotation for improved marker alignment.

--- src/components/SceneAR.vue
+++ src/components/SceneAR.vue
@@ -9,7 +9,7 @@
             gltf-model="/coin.glb"
             scale="0.2 0.2 0.2"
             position="0 0 0"
-            rotation="0 0 90"
+            rotation="90 90 90"
             animation-mixer>
         </a-entity>
       </a-marker>
